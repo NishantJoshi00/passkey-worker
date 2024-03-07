0b3578a9bfdc4de208f9938ace86888b495fdb9d
Nishant Joshi <nishantjo.12@gmail.com>
2024-03-08 01:16:12 +0530

    something more

---
+++ src/routes/protected.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import { getUserById } from "../file";
+
+const router = express.Router();
+
+// add middleware to check if user is logged in
+router.use(async (req, res, next) => {
+  if (req.session.loggedInUserId) {
+    const user = await getUserById(req.session.loggedInUserId);
+    if (user) {
+      return next();
+    }
+  }
+  res.status(401).json({ message: "Unauthorized" });
+});
+
+router.post("/lock", (req, res) => {
+  res.json({ message: "Locked" });
+});
+
+router.post("/restart", (req, res) => {
+  res.json({ message: "Restarted" });
+})
+
+export { router };
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,14 +1,30 @@
-import express from 'express';
-import { handleError } from '../middleware/errorHandler';
-import { handleRegisterStart, handleRegisterFinish } from '../controllers/registration';
-import { handleLoginStart, handleLoginFinish } from '../controllers/authentication';
+import express from "express";
+import { handleError } from "../middleware/errorHandler";
+import config from "../../config.json";
+import {
+  handleRegisterStart,
+  handleRegisterFinish,
+} from "../controllers/registration";
+import {
+  handleLoginStart,
+  handleLoginFinish,
+} from "../controllers/authentication";
 
 const router = express.Router();
 
-router.post('/registerStart', handleRegisterStart);
-router.post('/registerFinish', handleRegisterFinish);
-router.post('/loginStart', handleLoginStart);
-router.post('/loginFinish', handleLoginFinish);
+if (process.env.ALLOW_REGISTER || config.ALLOW_REGISTER) {
+  router.post("/registerStart", handleRegisterStart);
+  router.post("/registerFinish", handleRegisterFinish);
+}
+
+router.post("/loginStart", handleLoginStart);
+router.post("/loginFinish", handleLoginFinish);
+
+router.post("/logout", (req, res) => {
+  req.session.currentChallenge = undefined;
+  req.session.loggedInUserId = undefined;
+  res.status(200).json({ message: "Logged out" });
+});
 
 router.use(handleError);
 
